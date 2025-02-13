3dafdedb968f88a10a659e4ff26f81e3f05d2ccc
junon an <gaebalnam824@gmail.com>
2025-02-14 02:51:27 +0900

    fix: brand name change

--- src/main/java/com/musinsa/assignment/product/common/ProductInitializer.java
+++ src/main/java/com/musinsa/assignment/product/common/ProductInitializer.java
@@ -25,95 +25,95 @@ public class ProductInitializer {
             }
 
             List<Product> initialProducts = Arrays.asList(
-                // NIKE
-                createProduct("NIKE", Category.TOP, 11200),
-                createProduct("NIKE", Category.OUTER, 5500),
-                createProduct("NIKE", Category.PANTS, 4200),
-                createProduct("NIKE", Category.SNEAKERS, 9000),
-                createProduct("NIKE", Category.BAG, 2000),
-                createProduct("NIKE", Category.HAT, 1700),
-                createProduct("NIKE", Category.SOCKS, 1800),
-                createProduct("NIKE", Category.ACCESSORY, 2300),
+                // A
+                createProduct("A", Category.TOP, 11200),
+                createProduct("A", Category.OUTER, 5500),
+                createProduct("A", Category.PANTS, 4200),
+                createProduct("A", Category.SNEAKERS, 9000),
+                createProduct("A", Category.BAG, 2000),
+                createProduct("A", Category.HAT, 1700),
+                createProduct("A", Category.SOCKS, 1800),
+                createProduct("A", Category.ACCESSORY, 2300),
 
-                // ADIDAS
-                createProduct("ADIDAS", Category.TOP, 10500),
-                createProduct("ADIDAS", Category.OUTER, 5900),
-                createProduct("ADIDAS", Category.PANTS, 3800),
-                createProduct("ADIDAS", Category.SNEAKERS, 9100),
-                createProduct("ADIDAS", Category.BAG, 2100),
-                createProduct("ADIDAS", Category.HAT, 2000),
-                createProduct("ADIDAS", Category.SOCKS, 2000),
-                createProduct("ADIDAS", Category.ACCESSORY, 2200),
+                // B
+                createProduct("B", Category.TOP, 10500),
+                createProduct("B", Category.OUTER, 5900),
+                createProduct("B", Category.PANTS, 3800),
+                createProduct("B", Category.SNEAKERS, 9100),
+                createProduct("B", Category.BAG, 2100),
+                createProduct("B", Category.HAT, 2000),
+                createProduct("B", Category.SOCKS, 2000),
+                createProduct("B", Category.ACCESSORY, 2200),
 
-                // PUMA
-                createProduct("PUMA", Category.TOP, 10000),
-                createProduct("PUMA", Category.OUTER, 6200),
-                createProduct("PUMA", Category.PANTS, 3300),
-                createProduct("PUMA", Category.SNEAKERS, 9200),
-                createProduct("PUMA", Category.BAG, 2200),
-                createProduct("PUMA", Category.HAT, 1900),
-                createProduct("PUMA", Category.SOCKS, 2200),
-                createProduct("PUMA", Category.ACCESSORY, 2100),
+                // C
+                createProduct("C", Category.TOP, 10000),
+                createProduct("C", Category.OUTER, 6200),
+                createProduct("C", Category.PANTS, 3300),
+                createProduct("C", Category.SNEAKERS, 9200),
+                createProduct("C", Category.BAG, 2200),
+                createProduct("C", Category.HAT, 1900),
+                createProduct("C", Category.SOCKS, 2200),
+                createProduct("C", Category.ACCESSORY, 2100),
 
-                // FILA
-                createProduct("FILA", Category.TOP, 10100),
-                createProduct("FILA", Category.OUTER, 5100),
-                createProduct("FILA", Category.PANTS, 3000),
-                createProduct("FILA", Category.SNEAKERS, 9500),
-                createProduct("FILA", Category.BAG, 2500),
-                createProduct("FILA", Category.HAT, 1500),
-                createProduct("FILA", Category.SOCKS, 2400),
-                createProduct("FILA", Category.ACCESSORY, 2000),
+                // D
+                createProduct("D", Category.TOP, 10100),
+                createProduct("D", Category.OUTER, 5100),
+                createProduct("D", Category.PANTS, 3000),
+                createProduct("D", Category.SNEAKERS, 9500),
+                createProduct("D", Category.BAG, 2500),
+                createProduct("D", Category.HAT, 1500),
+                createProduct("D", Category.SOCKS, 2400),
+                createProduct("D", Category.ACCESSORY, 2000),
 
-                // REEBOK
-                createProduct("REEBOK", Category.TOP, 10700),
-                createProduct("REEBOK", Category.OUTER, 5000),
-                createProduct("REEBOK", Category.PANTS, 3800),
-                createProduct("REEBOK", Category.SNEAKERS, 9900),
-                createProduct("REEBOK", Category.BAG, 2300),
-                createProduct("REEBOK", Category.HAT, 1800),
-                createProduct("REEBOK", Category.SOCKS, 2100),
-                createProduct("REEBOK", Category.ACCESSORY, 2100),
+                // E
+                createProduct("E", Category.TOP, 10700),
+                createProduct("E", Category.OUTER, 5000),
+                createProduct("E", Category.PANTS, 3800),
+                createProduct("E", Category.SNEAKERS, 9900),
+                createProduct("E", Category.BAG, 2300),
+                createProduct("E", Category.HAT, 1800),
+                createProduct("E", Category.SOCKS, 2100),
+                createProduct("E", Category.ACCESSORY, 2100),
 
-                // UNDERARMOUR
-                createProduct("UNDERARMOUR", Category.TOP, 11200),
-                createProduct("UNDERARMOUR", Category.OUTER, 7200),
-                createProduct("UNDERARMOUR", Category.PANTS, 4000),
-                createProduct("UNDERARMOUR", Category.SNEAKERS, 9300),
-                createProduct("UNDERARMOUR", Category.BAG, 2100),
-                createProduct("UNDERARMOUR", Category.HAT, 1600),
-                createProduct("UNDERARMOUR", Category.SOCKS, 2300),
-                createProduct("UNDERARMOUR", Category.ACCESSORY, 1900),
+                // F
+                createProduct("F", Category.TOP, 11200),
+                createProduct("F", Category.OUTER, 7200),
+                createProduct("F", Category.PANTS, 4000),
+                createProduct("F", Category.SNEAKERS, 9300),
+                createProduct("F", Category.BAG, 2100),
+                createProduct("F", Category.HAT, 1600),
+                createProduct("F", Category.SOCKS, 2300),
+                createProduct("F", Category.ACCESSORY, 1900),
 
-                // NEWBALANCE
-                createProduct("NEWBALANCE", Category.TOP, 10500),
-                createProduct("NEWBALANCE", Category.OUTER, 5800),
-                createProduct("NEWBALANCE", Category.PANTS, 3900),
-                createProduct("NEWBALANCE", Category.SNEAKERS, 9000),
-                createProduct("NEWBALANCE", Category.BAG, 2200),
-                createProduct("NEWBALANCE", Category.HAT, 1700),
-                createProduct("NEWBALANCE", Category.SOCKS, 2100),
-                createProduct("NEWBALANCE", Category.ACCESSORY, 2000),
+                // G
+                createProduct("G", Category.TOP, 10500),
+                createProduct("G", Category.OUTER, 5800),
+                createProduct("G", Category.PANTS, 3900),
+                createProduct("G", Category.SNEAKERS, 9000),
+                createProduct("G", Category.BAG, 2200),
+                createProduct("G", Category.HAT, 1700),
+                createProduct("G", Category.SOCKS, 2100),
+                createProduct("G", Category.ACCESSORY, 2000),
 
-                // ASICS
-                createProduct("ASICS", Category.TOP, 10800),
-                createProduct("ASICS", Category.OUTER, 6300),
-                createProduct("ASICS", Category.PANTS, 3100),
-                createProduct("ASICS", Category.SNEAKERS, 9700),
-                createProduct("ASICS", Category.BAG, 2100),
-                createProduct("ASICS", Category.HAT, 1600),
-                createProduct("ASICS", Category.SOCKS, 2000),
-                createProduct("ASICS", Category.ACCESSORY, 2000),
+                // H
+                createProduct("H", Category.TOP, 10800),
+                createProduct("H", Category.OUTER, 6300),
+                createProduct("H", Category.PANTS, 3100),
+                createProduct("H", Category.SNEAKERS, 9700),
+                createProduct("H", Category.BAG, 2100),
+                createProduct("H", Category.HAT, 1600),
+                createProduct("H", Category.SOCKS, 2000),
+                createProduct("H", Category.ACCESSORY, 2000),
 
-                // CONVERSE
-                createProduct("CONVERSE", Category.TOP, 11400),
-                createProduct("CONVERSE", Category.OUTER, 6700),
-                createProduct("CONVERSE", Category.PANTS, 3200),
-                createProduct("CONVERSE", Category.SNEAKERS, 9500),
-                createProduct("CONVERSE", Category.BAG, 2400),
-                createProduct("CONVERSE", Category.HAT, 1700),
-                createProduct("CONVERSE", Category.SOCKS, 1700),
-                createProduct("CONVERSE", Category.ACCESSORY, 2400)
+                // I
+                createProduct("I", Category.TOP, 11400),
+                createProduct("I", Category.OUTER, 6700),
+                createProduct("I", Category.PANTS, 3200),
+                createProduct("I", Category.SNEAKERS, 9500),
+                createProduct("I", Category.BAG, 2400),
+                createProduct("I", Category.HAT, 1700),
+                createProduct("I", Category.SOCKS, 1700),
+                createProduct("I", Category.ACCESSORY, 2400)
             );
 
             productRepository.saveAll(initialProducts);
